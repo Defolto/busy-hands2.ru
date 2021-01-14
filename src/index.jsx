0c403b0e0f7d39f2b4213db16ebe01e67d3cde62
Defolto <37343435+Defolto@users.noreply.github.com>
2021-01-14 15:16:27 +0300

    созданы нужные директории + scss
    созданы папки для необходимых файлов
    подключена поддержка scss и react

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,12 @@
-console.log("hello world");
+import React, { Fragment } from 'react'
+import {render} from 'react-dom'
+import './styles/general.scss'
+
+class BusyHands extends React.Component{
+    render(){
+        console.log(5);
+        return <p>Привет мир!</p>
+    }
+}
+
+render(<BusyHands />, document.querySelector('#Busy-hands'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: {
-        main: './index.jsx'
+        main: ['@babel/polyfill','./index.jsx']
     },
     output: {
         filename: "[name].[contenthash].js",
@@ -17,6 +18,33 @@ module.exports = {
             title: "Busy-hands",
             template: './index.html'
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css"
+        }),
     ],
+    module:{
+        rules:[
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options:{
+                        publicPath: ''
+                    },
+                }, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                    plugins: ['@babel/plugin-proposal-class-properties']
+                }
+                }
+            },
+        ]
+    }
 }
